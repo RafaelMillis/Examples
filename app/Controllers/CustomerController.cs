a0c60a1a024215cc3c48de319326dd90cc57e3fe
RafaelMillis <42179306+RafaelMillis@users.noreply.github.com>
2018-10-18 02:57:28 -0500

    Add files via upload

---
+++ app/Controllers/CustomerController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TestModels;
+using TestRepository;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TestWebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    public class CustomerController : Controller
+    {
+
+        [HttpGet("[action]")]
+        public List<Customer> All()
+        {
+            Repo repo = new Repo();
+            return repo.GetCustomers();
+        }
+    }
+}
---
+++ app/Repo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using TestModels;
+
+namespace TestRepository
+{
+    public class Repo
+    {
+        public List<Customer> GetCustomers()
+        {
+            return LoadJson("data/customers.json");
+        }
+
+        public List<Customer> LoadJson(string fileName)
+        {
+            using (StreamReader r = new StreamReader(fileName))
+            {
+                string json = r.ReadToEnd();
+                Customers = JsonConvert.DeserializeObject<List<Customer>>(json);
+            }
+
+            return Customers;
+        }
+
+
+        List<Customer> Customers;
+    }
+
+}
