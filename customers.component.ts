ce32aa81f5092f9679ee6c222f00aac5f9bd2d09
RafaelMillis <42179306+RafaelMillis@users.noreply.github.com>
2018-10-18 02:47:11 -0500

    Add files via upload

---
+++ customers.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+//import { HttpClient } from 'selenium-webdriver/http';
+import { Customer } from '../interfaces/customer' 
+import { DataService } from '../services/data/data.service'
+
+@Component({
+  selector: 'app-customers',
+  templateUrl: './customers.component.html',
+  styleUrls: ['./customers.component.css']
+})
+export class CustomersComponent implements OnInit {
+  customers: Customer[];
+  public selectedCustomer: Customer;
+  constructor( private dataService: DataService) { }
+
+  ngOnInit() {
+    this.getCustomerData();
+  }
+
+  getCustomerData(): void {
+    this.dataService.getCustomersData().subscribe(customers => this.customers = customers);
+  }
+
+  onSelect(customer: Customer): void {
+    this.selectedCustomer = customer;
+    console.log("customer :" + customer.id + " " + customer.name);
+  }
+}
